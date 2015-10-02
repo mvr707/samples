d222c069bd823ca57e15baaab0421321b5e691fc
Venkat <mvr707@gmail.com>
2015-10-02 14:04:41 -0700

    update

--- perl6/bingo-v5v6/v5.pl
+++ perl6/bingo-v5v6/v5.pl
@@ -8,61 +8,61 @@ $Data::Dumper::Sortkeys = 1;
 my $game = 0;
 my $draws = {};
 for ([5,27,46,55,67], [14,23,32,47,62], [39,45,44,42,35], [69,49,1,26,10], [74,61,68,73,69]) {
-		my $h; 
-		@$h{@$_} = @$_; 
-		$game++;
-		$draws ->{$game} = $h; 
+  my $h; 
+  @$h{@$_} = @$_; 
+  $game++;
+  $draws ->{$game} = $h; 
 }
 
 my $winners;
 
 sub examine
 {
-	my %in = @_;
-	while (my ($k, $v) = each %in) {
-		my @bingo = @$v;
-		map { push(@{$winners->{$_}}, $k) if (@bingo == grep { defined } @{$draws->{$_}}{@bingo}) } keys %$draws;
-	}
+ my %in = @_;
+ while (my ($k, $v) = each %in) {
+  my @bingo = @$v;
+  map { push(@{$winners->{$_}}, $k) if (@bingo == grep { defined } @{$draws->{$_}}{@bingo}) } keys %$draws;
+ }
 }
 
 ### Parse Block
 {
-	open (my $fh, '<', "bingo.txt") or die;
-	my $t = [];
-	my ($player, $board);
+ open (my $fh, '<', "bingo.txt") or die;
+ my $t = [];
+ my ($player, $board);
 
-	while (my $line = <$fh>) {
-		next if ($line =~ /^\s*$/);
-		chomp($line);
-		if ($line =~ /Player:\s*(.*)\s*$/) {
-			$player = $1;
-			my $tmp = <$fh>;
-			if ($tmp =~ /Board:\s*(.*)\s*$/) {
-				$board = $1;
-			}
-			my $id = "$board:$player";
+ while (my $line = <$fh>) {
+  next if ($line =~ /^\s*$/);
+  chomp($line);
+  if ($line =~ /Player:\s*(.*)\s*$/) {
+   $player = $1;
+   my $tmp = <$fh>;
+   if ($tmp =~ /Board:\s*(.*)\s*$/) {
+    $board = $1;
+   }
+   my $id = "$board:$player";
 
-			examine("${id}:h1" => $t->[0],
-				"${id}:h2" => $t->[1],
-				"${id}:h3" => $t->[2],
-				"${id}:h4" => $t->[3],
-				"${id}:h5" => $t->[4],
-			
-				"${id}:v1" => [ $t->[0][0],$t->[1][0],$t->[2][0],$t->[3][0],$t->[4][0] ],
-				"${id}:v2" => [ $t->[0][1],$t->[1][1],$t->[2][1],$t->[3][1],$t->[4][1] ],
-				"${id}:v3" => [ $t->[0][2],$t->[1][2],$t->[3][2],$t->[4][2] ],
-				"${id}:v4" => [ $t->[0][3],$t->[1][3],$t->[2][2],$t->[3][3],$t->[4][3] ],
-				"${id}:v5" => [ $t->[0][4],$t->[1][4],$t->[2][3],$t->[3][4],$t->[4][4] ],
+   examine("${id}:h1" => $t->[0],
+    "${id}:h2" => $t->[1],
+    "${id}:h3" => $t->[2],
+    "${id}:h4" => $t->[3],
+    "${id}:h5" => $t->[4],
+   
+    "${id}:v1" => [ $t->[0][0],$t->[1][0],$t->[2][0],$t->[3][0],$t->[4][0] ],
+    "${id}:v2" => [ $t->[0][1],$t->[1][1],$t->[2][1],$t->[3][1],$t->[4][1] ],
+    "${id}:v3" => [ $t->[0][2],$t->[1][2],$t->[3][2],$t->[4][2] ],
+    "${id}:v4" => [ $t->[0][3],$t->[1][3],$t->[2][2],$t->[3][3],$t->[4][3] ],
+    "${id}:v5" => [ $t->[0][4],$t->[1][4],$t->[2][3],$t->[3][4],$t->[4][4] ],
 
-				"${id}:df" => [ $t->[0][0],$t->[1][1],$t->[3][3],$t->[4][4] ],
-				"${id}:db" => [ $t->[0][4],$t->[1][3],$t->[3][1],$t->[4][0] ]);
+    "${id}:df" => [ $t->[0][0],$t->[1][1],$t->[3][3],$t->[4][4] ],
+    "${id}:db" => [ $t->[0][4],$t->[1][3],$t->[3][1],$t->[4][0] ]);
 
-			$t = [];
-		} else {
-			push(@$t, [grep { /\d/ } split(' ', $line)]);
-		}
-	}
-	close($fh);
+   $t = [];
+  } else {
+   push(@$t, [grep { /\d/ } split(' ', $line)]);
+  }
+ }
+ close($fh);
 }
 
 print Data::Dumper->Dump([$winners], ['Winners']);
--- perl6/bingo-v5v6/v6.pl
+++ perl6/bingo-v5v6/v6.pl
@@ -4,65 +4,65 @@ my $game = 0;
 my %draws;
 
 for (5,27,46,55,67), (14,23,32,47,62), (39,45,44,42,35), (69,49,1,26,10), (74,61,68,73,69) {
-		my %h; 
-		%h{$_} = $_; 
-		$game++;
-		%draws{$game} = %h; 
+  my %h; 
+  %h{$_} = $_; 
+  $game++;
+  %draws{$game} = %h; 
 }
 
 my %winners;
 
 sub examine(%in)
 {
-	for %in.kv -> $id, $bingo {
-		# %draws.keys.map({ %winners{$_}.push($id) if ($bingo.elems == %draws{$_}{@($bingo)}.grep({$_.defined}).elems) });
-		
-		for %draws.keys -> $g {
-			if ($bingo.elems == %draws{$g}{@($bingo)}.grep({$_.defined}).elems) {
-				%winners{$g}.push($id);
-			}
-		}
-	}
+ for %in.kv -> $id, $bingo {
+  # %draws.keys.map({ %winners{$_}.push($id) if ($bingo.elems == %draws{$_}{@($bingo)}.grep({$_.defined}).elems) });
+  
+  for %draws.keys -> $g {
+   if ($bingo.elems == %draws{$g}{@($bingo)}.grep({$_.defined}).elems) {
+    %winners{$g}.push($id);
+   }
+  }
+ }
 }
 
 ### Parse Block
 {
-	my $fh = open "bingo.txt", :r;
-	my @t = ();
-	my ($player, $board);
+ my $fh = open "bingo.txt", :r;
+ my @t = ();
+ my ($player, $board);
 
-	while (defined my $line = $fh.get) {
-		next if ($line ~~ /^\s*$/);
-		$line = $line.trim;
-		if ($line ~~ /Player/) {
-			$player = $line.split(':')[1];
-			my $tmp = $fh.get;
-			if ($tmp ~~ /Board/) {
-				$board = $tmp.split(':')[1];
-			}
-			my $id = "{$board}:{$player}";
+ while (defined my $line = $fh.get) {
+  next if ($line ~~ /^\s*$/);
+  $line = $line.trim;
+  if ($line ~~ /Player/) {
+   $player = $line.split(':')[1];
+   my $tmp = $fh.get;
+   if ($tmp ~~ /Board/) {
+    $board = $tmp.split(':')[1];
+   }
+   my $id = "{$board}:{$player}";
 
-			examine({"{$id}:h1" => @t[0],
-				"{$id}:h2" => @t[1],
-				"{$id}:h3" => @t[2],
-				"{$id}:h4" => @t[3],
-				"{$id}:h5" => @t[4],
-			
-				"{$id}:v1" => [ @t[0][0],@t[1][0],@t[2][0],@t[3][0],@t[4][0] ],
-				"{$id}:v2" => [ @t[0][1],@t[1][1],@t[2][1],@t[3][1],@t[4][1] ],
-				"{$id}:v3" => [ @t[0][2],@t[1][2],@t[3][2],@t[4][2] ],
-				"{$id}:v4" => [ @t[0][3],@t[1][3],@t[2][2],@t[3][3],@t[4][3] ],
-				"{$id}:v5" => [ @t[0][4],@t[1][4],@t[2][3],@t[3][4],@t[4][4] ],
+   examine({"{$id}:h1" => @t[0],
+    "{$id}:h2" => @t[1],
+    "{$id}:h3" => @t[2],
+    "{$id}:h4" => @t[3],
+    "{$id}:h5" => @t[4],
+   
+    "{$id}:v1" => [ @t[0][0],@t[1][0],@t[2][0],@t[3][0],@t[4][0] ],
+    "{$id}:v2" => [ @t[0][1],@t[1][1],@t[2][1],@t[3][1],@t[4][1] ],
+    "{$id}:v3" => [ @t[0][2],@t[1][2],@t[3][2],@t[4][2] ],
+    "{$id}:v4" => [ @t[0][3],@t[1][3],@t[2][2],@t[3][3],@t[4][3] ],
+    "{$id}:v5" => [ @t[0][4],@t[1][4],@t[2][3],@t[3][4],@t[4][4] ],
 
-				"{$id}:df" => [ @t[0][0],@t[1][1],@t[3][3],@t[4][4] ],
-				"{$id}:db" => [ @t[0][4],@t[1][3],@t[3][1],@t[4][0] ]});
+    "{$id}:df" => [ @t[0][0],@t[1][1],@t[3][3],@t[4][4] ],
+    "{$id}:db" => [ @t[0][4],@t[1][3],@t[3][1],@t[4][0] ]});
 
-			@t = ();
-		} else {
-			push(@t, [$line.split(' ').grep(/\d/)]);
-		}
-	}
-	$fh.close;
+   @t = ();
+  } else {
+   push(@t, [$line.split(' ').grep(/\d/)]);
+  }
+ }
+ $fh.close;
 }
 
 %winners.say;
