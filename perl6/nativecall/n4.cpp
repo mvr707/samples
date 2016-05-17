7b266af408bb1622e7690a40fea5503f403359f1
Ramana Mokkapati <mvr707@gmail.com>
2016-05-17 16:32:44 -0700

    update

--- perl6/nativecall/n4.cpp
+++ perl6/nativecall/n4.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
+using namespace std;
 
-double My_variable = 3.0;
+double my_variable = 3.5;
 
 int hello(int i)
 {
-	std::cout << "Hello World from C++ " << i << std::endl;
+	cout << "Hello World from C++ " << i << endl;
 	i++;
 	return i;
 }
+
+class Rectangle {
+	int width, height;
+
+	public:
+		void set_values (int,int);
+		int area();
+};
+
+
+void Rectangle::set_values (int x, int y) {
+
+	// cout << "width = " << x << endl;
+	// cout << "height = " << y << endl;
+
+	width = x;
+	height = y;
+
+}
+
+int Rectangle::area() {
+
+	// cout << "width = " << width << endl;
+	// cout << "height = " << height << endl;
+
+	return width * height;
+}
+
--- perl6/nativecall/n4.p6
+++ perl6/nativecall/n4.p6
@@ -1,7 +1,7 @@
 use NativeCall;
 
 
-my $var = cglobal('./libn4.so', 'My_variable', num64);
+my $var = cglobal('./libn4.so', 'my_variable', num64);
 
 sub hello(int32) returns int32 is native('./libn4.so') is mangled { ... };
 
@@ -10,3 +10,18 @@ my $ret = hello(1234);
 say "return value is $ret";
 
 say $var + 10;
+
+class Rectangle is repr<CPPStruct> {
+	has int32 $.width;
+	has int32 $.height;
+
+	method set_values(int32, int32) is native('./libn4.so') { ... };
+	method area() returns int32 is native('./libn4.so') { ... };
+};
+
+my $r = Rectangle.new;
+
+$r.set_values(3,4);
+
+say "area = { $r.area() }";
+
