b235f7ee6e3a8b63f70224b4bc55979f6c81adb5
Ramana Mokkapati <mvr707@gmail.com>
2016-05-17 15:37:44 -0700

    update

--- perl6/nativecall/n3.c
+++ perl6/nativecall/n3.c
@@ -1,12 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+/* ================================================== */
 
 double My_variable = 3.0;
+
+/* ================================================== */
 
 int fact(int n) {
 	if (n <= 1) return 1;
 	else return n*fact(n-1);
 }
 
+/* ================================================== */
+
 int my_mod(int n, int m) {
 	return (n % m);
 }
+
+/* ================================================== */
+
+char *get_name()
+{
+	static char buf[100];
+
+	int i = rand();
+	sprintf(buf, "all is well %d", i);
+
+	return buf;
+}
+
+/* ================================================== */
+
+typedef struct _SumDiff
+{
+	int sum;
+	int diff;
+} SumDiff;
+
+SumDiff calculate_sum(int i, int j) {
+	SumDiff res;
+	res.sum = i + j;
+	return res;
+}
+
+SumDiff calculate_diff(int i, int j) {
+	SumDiff res;
+	res.diff = abs(i - j);
+	return res;
+}
+
+SumDiff *sumdiff (int i, int j) {
+	SumDiff *result = calloc(1, sizeof(SumDiff));	/* static SumDiff result; */
+	SumDiff tmp;
+
+	tmp = calculate_sum(i, j);
+	result->sum = tmp.sum;				/* result.sum = tmp.sum; */
+
+	tmp = calculate_diff(i,j);
+	result->diff = tmp.diff;			/* result.diff = tmp.diff; */
+
+	return result;					/* return &result; */
+}
+	/* Notes: 
+	   1) Internally C function can return structured data to another C function, 
+	      but must return structure pointer to Perl6. Why? May be that is the current implementation 
+	   2) For data integrity, "calloc" is better than "static" declaration of "result"
+	   
+	*/
+
+/* ================================================== */
+
+char **get_list(int size)
+{
+	char **my_array = calloc(size, sizeof(char *));
+
+	for (int i=0; i < size; i++) {
+		char buf[100];
+		sprintf(buf, "all is well %d", i);
+  		my_array[i] = calloc(strlen(buf), 1);
+		sprintf(my_array[i], "%s", buf);
+	}
+	return my_array;
+}
+
+/* ================================================== */
--- perl6/nativecall/n3.p6
+++ perl6/nativecall/n3.p6
@@ -1,13 +1,65 @@
 use NativeCall;
 
+##################################################################
+
 my $var = cglobal('./libn3.so', 'My_variable', num64);
-
-sub fact(int32) returns int32 is native('./libn3.so') { };
-
-sub my-mod(int32, int32) returns int32 is symbol('my_mod') is native('./libn3.so') { };
-
-say fact(4);
-
-say my-mod 23, 7;
-
 say $var + 4.5;
+say "---";
+
+##################################################################
+
+sub fact(int32) returns int32 
+	is native('./libn3.so') { };
+say fact(4);
+say "---";
+
+##################################################################
+
+sub my-mod(int32, int32) returns int32 
+	is symbol('my_mod') 
+	is native('./libn3.so') { };
+say my-mod 23, 7;
+say "---";
+
+##################################################################
+
+sub get-name() returns Str
+	is symbol('get_name')
+	is native('./libn3.so') { };
+my $n1 = get-name();
+my $n2 = get-name();
+my $n3 = get-name();
+say $n1;
+say $n2;
+say $n3;
+say "---";
+
+##################################################################
+
+my class SumDiff is repr('CStruct') {
+	has int32 $.sum;
+	has int32 $.diff;
+};
+sub sumdiff(int32, int32) returns SumDiff
+	is native('./libn3.so') { };
+my $result = SumDiff.new;
+say $result;
+say nativesizeof($result);
+my $res = sumdiff(4,50);
+say $res;
+say "sum is ", $res.sum;
+say "diff is ", $res.diff;
+say "---";
+
+##################################################################
+
+sub get-list(int32) returns CArray[Str]
+	is symbol('get_list')
+	is native('./libn3.so') { };
+
+my $length = 3;
+
+my CArray[Str] $list = get-list($length);
+say nativesizeof($list);
+
+for ^$length { say $list[$_] };
